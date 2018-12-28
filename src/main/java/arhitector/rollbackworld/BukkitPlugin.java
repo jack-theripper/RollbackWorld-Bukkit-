5cda551ddd939206775d5b2a29f8560ad99ad739
Dmitry Arhitector <dmitry.arhitector@yandex.ru>
2018-12-28 19:28:56 +0300

    First init

---
+++ src/main/java/arhitector/rollbackworld/BukkitPlugin.java
@@ -0,0 +1,18 @@
+package arhitector.rollbackworld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BukkitPlugin extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+	
+}
