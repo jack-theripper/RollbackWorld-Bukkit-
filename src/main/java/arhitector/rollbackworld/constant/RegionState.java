49a8dfd8722fa646421701d8c7edd6675deb2f75
Dmitry Arhitector <dmitry.arhitector@yandex.ru>
2018-12-31 11:06:26 +0300

    State constants

---
+++ src/main/java/arhitector/rollbackworld/constant/RegionState.java
@@ -0,0 +1,18 @@
+package arhitector.rollbackworld.constant;
+
+/**
+ * State constants.
+ */
+public enum RegionState {
+	
+	/**
+	 * The area is in the queue
+	 */
+	QUEUED,
+	
+	/**
+	 * The region is in a rollback state
+	 */
+	ROLLBACKING;
+	
+}
